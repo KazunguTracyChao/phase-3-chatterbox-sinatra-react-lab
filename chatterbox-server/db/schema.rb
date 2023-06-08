4c5f07941690a11d63d85057e060d420b6039fe9
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-06-09 01:37:54 +0300

    Updated assignment

--- chatterbox-server/db/schema.rb
+++ chatterbox-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_08_180803) do
+ActiveRecord::Schema.define(version: 2023_06_08_221436) do
 
   create_table "electronics", force: :cascade do |t|
     t.string "name"
